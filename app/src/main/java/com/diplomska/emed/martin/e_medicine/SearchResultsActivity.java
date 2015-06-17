3339fbbe20e922400d3e820b2791fcdaba7c2b57
martin04 <martin04@t-home.mk>
2015-06-17 12:01:11 +0200

    Implemented search, added two more options Alarms & About, added new colors to the app theme

--- app/src/main/java/com/diplomska/emed/martin/e_medicine/SearchResultsActivity.java
+++ app/src/main/java/com/diplomska/emed/martin/e_medicine/SearchResultsActivity.java
@@ -4,22 +4,25 @@ package com.diplomska.emed.martin.e_medicine;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.widget.Toast;
 
-
-
-public class MainActivity extends ActionBarActivity {
-
+/**
+ * Created by Martin on 17-Jun-15.
+ */
+public class SearchResultsActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
+        getSupportActionBar().setTitle(R.string.search_result);
+        handleIntent(getIntent());
     }
 
     @Override
@@ -35,16 +38,14 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
 
-        int id = item.getItemId();
-
-
-        if (id == R.id.action_settings ) {
-            return true;
+    private void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query=intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this,query,Toast.LENGTH_LONG).show();
         }
-
-
-        return super.onOptionsItemSelected(item);
     }
 }
