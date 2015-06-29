2936a444a01adc3a650c43c7cb409ac75f355540
martin04 <martin04@t-home.mk>
2015-06-29 09:49:08 +0200

    Implemented search, added two more options Alarms & About, added new colors to the app theme, added ABOUT,started developing the DataBase, made helper, models, adapters,asynctask for loading db,recycler view DONE, db select and show drug names works, changed the splash screen image.

--- app/src/main/java/com/diplomska/emed/martin/e_medicine/adapter/DrugNameAdapter.java
+++ app/src/main/java/com/diplomska/emed/martin/e_medicine/adapter/DrugNameAdapter.java
@@ -20,19 +20,19 @@ public class DrugNameAdapter extends RecyclerView.Adapter<DrugNameViewHolder> {
     List<Drug> drugsDataSet;
 
     public DrugNameAdapter(List<Drug> drugs) {
-        drugsDataSet=drugs;
+        drugsDataSet = drugs;
     }
 
     @Override
     public DrugNameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
-        View view=inflater.inflate(R.layout.list_item_layout,parent,false);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.list_item_layout, parent, false);
         return new DrugNameViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(DrugNameViewHolder holder, int position) {
-        Drug d=drugsDataSet.get(position);
+        Drug d = drugsDataSet.get(position);
         holder.genericName.setText(d.getGeneric_name());
         holder.latinName.setText(d.getLatin_name());
     }
--- app/src/main/java/com/diplomska/emed/martin/e_medicine/holders/DrugNameViewHolder.java
+++ app/src/main/java/com/diplomska/emed/martin/e_medicine/holders/DrugNameViewHolder.java
@@ -1,27 +1,38 @@
 package com.diplomska.emed.martin.e_medicine.holders;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.diplomska.emed.martin.e_medicine.R;
 
 /**
  * Created by Martin on 26-Jun-15.
  */
-public class DrugNameViewHolder extends RecyclerView.ViewHolder {
+public class DrugNameViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     //I use this to find all the views in the list_item_layout.xml
 
-    //public ImageView pills;
-    //public ImageView alarm;
+    public ImageView alarm;
     public TextView genericName;
     public TextView latinName;
 
     public DrugNameViewHolder(View itemView) {
         super(itemView);
-        //pills=(ImageView)itemView.findViewById(R.id.imgPill);
-        //alarm=(ImageView)itemView.findViewById(R.id.imgAlarm);
+        
+        alarm=(ImageView)itemView.findViewById(R.id.imgAlarm);
         genericName = (TextView) itemView.findViewById(R.id.txtGeneric);
         latinName = (TextView) itemView.findViewById(R.id.txtLatin);
+
+        alarm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Context ctx=itemView.getContext();
+        int pos=getPosition();
+        Toast.makeText(ctx,"Button "+pos+ " Clicked",Toast.LENGTH_SHORT).show();
     }
 }
